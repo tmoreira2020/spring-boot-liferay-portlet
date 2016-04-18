ac9f543eb5e022a793189b8f09a80f4a78094289
Thiago Leão Moreira <tmoreira2020@gmail.com>
2016-04-18 15:46:22 -0300

    Enable Spring Boot
    This commis enable Spring Boot which ends up breaking the flow action -> render phase of Liferay. How?
    After the correct processing of an action phase Liferay render a blank page, looks like that it doesn't know
    which page it must render even though the parameter "render" is set to "alternative-view". For some
    reason Spring Boot break the process without any exception or clue.

--- src/main/java/com/liferay/springboot/ApplicationConfiguration.java
+++ src/main/java/com/liferay/springboot/ApplicationConfiguration.java
@@ -3,12 +3,13 @@ package com.liferay.springboot;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-public class ApplicationConfiguration {
+public class ApplicationConfiguration extends SpringBootServletInitializer {
 
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ApplicationConfiguration.class);
